559a0fd5949c2b3b8aacd07ab12ddb81aa6cba56
wangyunjing <1369538404@qq.com>
2018-05-11 01:53:06 +0800

    update abstarctInstanceConfig

--- src/main/java/com/wyj/guard/info/config/AbstractInstanceConfig.java
+++ src/main/java/com/wyj/guard/info/config/AbstractInstanceConfig.java
@@ -1,12 +1,13 @@
 package com.wyj.guard.info.config;
 
+import com.wyj.guard.share.enums.LaunchStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * 方便扩展
  */
-public abstract class AbstractInstanceConfig implements ConfigurableInstanceConfig{
+public abstract class AbstractInstanceConfig implements ConfigurableInstanceConfig {
 
     public final Logger logger = LoggerFactory.getLogger(AbstractInstanceConfig.class);
 
@@ -21,5 +22,149 @@ public abstract class AbstractInstanceConfig implements ConfigurableInstanceConf
         this.instanceId = instanceId;
     }
 
+    @Override
+    public final Integer getApplicationId() {
+        return applicationConfig.getApplicationId();
+    }
 
+    @Override
+    public final String getApplicationName() {
+        return applicationConfig.getApplicationName();
+    }
+
+
+    @Override
+    public final String getInstanceId() {
+        return this.instanceId;
+    }
+
+    @Override
+    public final Integer getPort() {
+        Integer port = doGetPort();
+
+        port = port != null ? port : applicationConfig.getDefaultPort();
+
+        if (port == null) {
+            // TODO: 2018/1/25
+            throw new RuntimeException("没有设置端口号");
+        }
+        return port;
+    }
+
+    protected abstract Integer doGetPort();
+
+    @Override
+    public final String getStartCommand() {
+        String startCommand = doGetStartCommand();
+
+        startCommand = startCommand != null ? startCommand : applicationConfig.getDefaultStartCommand();
+
+        if (startCommand == null) {
+            // TODO: 2018/1/25
+            throw new RuntimeException("没有设置启动命令");
+        }
+        return startCommand;
+    }
+
+    protected abstract String doGetStartCommand();
+
+    @Override
+    public final String getHealthUrl() {
+        String healthUrl = doGetHealthUrl();
+
+        healthUrl = healthUrl != null ? healthUrl : applicationConfig.getDefaultHealthUrl();
+
+        if (healthUrl == null) {
+            // TODO: 2018/1/25
+            throw new RuntimeException("没有设置检查URL");
+        }
+        return healthUrl;
+    }
+
+    protected abstract String doGetHealthUrl();
+
+    @Override
+    public final Integer getWeight() {
+        Integer weight = doGetWeight();
+
+        weight = weight != null ? weight : 0;
+
+        return weight;
+    }
+
+    protected abstract Integer doGetWeight();
+
+    @Override
+    public final String getUsername() {
+        String username = doGetUsername();
+
+        username = username != null ? username : applicationConfig.getDefaultUsername();
+
+        if (username == null) {
+            // TODO: 2018/1/25
+            throw new RuntimeException("没有设置远程服务器的用户名");
+        }
+        return username;
+    }
+
+    protected abstract String doGetUsername();
+
+    @Override
+    public final String getPassword() {
+        String password = doGetPassword();
+
+        password = password != null ? password : applicationConfig.getDefaultPassword();
+
+        if (password == null) {
+            // TODO: 2018/1/25
+            throw new RuntimeException("没有设置远程服务器的密码");
+        }
+        return password;
+    }
+
+    protected abstract String doGetPassword();
+
+    @Override
+    public final Integer getHeartbeatRate() {
+        Integer heartbeatRate = doGetHeartbeatRate();
+
+        heartbeatRate = heartbeatRate != null ? heartbeatRate : applicationConfig.getDefaultHeartbeatRate();
+
+        return heartbeatRate;
+    }
+
+    protected abstract Integer doGetHeartbeatRate();
+
+    @Override
+    public final Integer getInitializeInstanceDuration() {
+        Integer initializeInstanceDuration = doGetInitializeInstanceDuration();
+
+        initializeInstanceDuration = initializeInstanceDuration != null ? initializeInstanceDuration : applicationConfig.getDefaultInitializeInstanceDuration();
+
+        return initializeInstanceDuration;
+    }
+
+    protected abstract Integer doGetInitializeInstanceDuration();
+
+    @Override
+    public final Integer getSelfProtectedDuration() {
+        Integer selfProtectedDuration = doGetSelfProtectedDuration();
+
+        selfProtectedDuration = selfProtectedDuration != null ? selfProtectedDuration : applicationConfig.getDefaultSelfProtectedDuration();
+
+        return selfProtectedDuration;
+    }
+
+    protected abstract Integer doGetSelfProtectedDuration();
+
+    @Override
+    public final LaunchStatus getStatus() {
+        LaunchStatus applicationConfigStatus = applicationConfig.getStatus();
+        if (applicationConfigStatus.equals(LaunchStatus.SHUTDOWN)) {
+            return LaunchStatus.SHUTDOWN;
+        }
+        return doGetStatus();
+    }
+
+    protected abstract LaunchStatus doGetStatus();
 }
--- src/main/java/com/wyj/guard/info/config/DBInstanceConfig.java
+++ src/main/java/com/wyj/guard/info/config/DBInstanceConfig.java
@@ -31,21 +31,6 @@ public class DBInstanceConfig extends AbstractInstanceConfig {
     }
 
     @Override
-    public Integer getApplicationId() {
-        return applicationConfig.getApplicationId();
-    }
-
-    @Override
-    public String getApplicationName() {
-        return applicationConfig.getApplicationName();
-    }
-
-    @Override
-    public String getInstanceId() {
-        return this.instanceId;
-    }
-
-    @Override
     public String getIp() {
         String sql = StringPlaceholderResolver.resolvePlaceholder(DEFAULT_QUERY_PLACEHOLDER_SQL, "ip");
 
@@ -59,7 +44,7 @@ public class DBInstanceConfig extends AbstractInstanceConfig {
     }
 
     @Override
-    public Integer getPort() {
+    protected Integer doGetPort() {
         String sql = StringPlaceholderResolver.resolvePlaceholder(DEFAULT_QUERY_PLACEHOLDER_SQL, "port");
 
         Integer port = jdbcTemplate.query(sql, args, (resultSet) -> {
@@ -68,38 +53,23 @@ public class DBInstanceConfig extends AbstractInstanceConfig {
             }
             return null;
         });
-
-        port = port != null ? port : applicationConfig.getDefaultPort();
-
-        if (port == null) {
-            // TODO: 2018/1/25
-            throw new RuntimeException("没有设置端口号");
-        }
         return port;
     }
 
     @Override
-    public String getStartCommand() {
+    protected String doGetStartCommand() {
         String sql = StringPlaceholderResolver.resolvePlaceholder(DEFAULT_QUERY_PLACEHOLDER_SQL, "start_command");
-
         String startCommand = jdbcTemplate.query(sql, args, (resultSet) -> {
             if (resultSet.next()) {
                 return resultSet.getString(1);
             }
             return null;
         });
-
-        startCommand = startCommand != null ? startCommand : applicationConfig.getDefaultStartCommand();
-
-        if (startCommand == null) {
-            // TODO: 2018/1/25
-            throw new RuntimeException("没有设置启动命令");
-        }
         return startCommand;
     }
 
     @Override
-    public String getHealthUrl() {
+    protected String doGetHealthUrl() {
         String sql = StringPlaceholderResolver.resolvePlaceholder(DEFAULT_QUERY_PLACEHOLDER_SQL, "health_url");
 
         String healthUrl = jdbcTemplate.query(sql, args, (resultSet) -> {
@@ -108,18 +78,11 @@ public class DBInstanceConfig extends AbstractInstanceConfig {
             }
             return null;
         });
-
-        healthUrl = healthUrl != null ? healthUrl : applicationConfig.getDefaultHealthUrl();
-
-        if (healthUrl == null) {
-            // TODO: 2018/1/25
-            throw new RuntimeException("没有设置检查URL");
-        }
         return healthUrl;
     }
 
     @Override
-    public Integer getWeight() {
+    protected Integer doGetWeight() {
         String sql = StringPlaceholderResolver.resolvePlaceholder(DEFAULT_QUERY_PLACEHOLDER_SQL, "weight");
 
         Integer weight = jdbcTemplate.query(sql, args, (resultSet) -> {
@@ -128,14 +91,11 @@ public class DBInstanceConfig extends AbstractInstanceConfig {
             }
             return null;
         });
-
-        weight = weight != null ? weight : 0;
-
         return weight;
     }
 
     @Override
-    public String getUsername() {
+    protected String doGetUsername() {
         String sql = StringPlaceholderResolver.resolvePlaceholder(DEFAULT_QUERY_PLACEHOLDER_SQL, "username");
 
         String username = jdbcTemplate.query(sql, args, (resultSet) -> {
@@ -144,18 +104,11 @@ public class DBInstanceConfig extends AbstractInstanceConfig {
             }
             return null;
         });
-
-        username = username != null ? username : applicationConfig.getDefaultUsername();
-
-        if (username == null) {
-            // TODO: 2018/1/25
-            throw new RuntimeException("没有设置远程服务器的用户名");
-        }
         return username;
     }
 
     @Override
-    public String getPassword() {
+    protected String doGetPassword() {
         String sql = StringPlaceholderResolver.resolvePlaceholder(DEFAULT_QUERY_PLACEHOLDER_SQL, "password");
 
         String password = jdbcTemplate.query(sql, args, (resultSet) -> {
@@ -164,14 +117,58 @@ public class DBInstanceConfig extends AbstractInstanceConfig {
             }
             return null;
         });
-
-        password = password != null ? password : applicationConfig.getDefaultPassword();
-
-        if (password == null) {
-            // TODO: 2018/1/25
-            throw new RuntimeException("没有设置远程服务器的密码");
-        }
         return password;
+    }
+
+    @Override
+    protected Integer doGetHeartbeatRate() {
+        String sql = StringPlaceholderResolver.resolvePlaceholder(DEFAULT_QUERY_PLACEHOLDER_SQL, "heartbeat_rate");
+
+        Integer heartbeatRate = jdbcTemplate.query(sql, args, (resultSet) -> {
+            if (resultSet.next()) {
+                return resultSet.getInt(1);
+            }
+            return null;
+        });
+        return heartbeatRate;
+    }
+
+    @Override
+    protected Integer doGetInitializeInstanceDuration() {
+        String sql = StringPlaceholderResolver.resolvePlaceholder(DEFAULT_QUERY_PLACEHOLDER_SQL, "initialize_instance_duration");
+
+        Integer initializeInstanceDuration = jdbcTemplate.query(sql, args, (resultSet) -> {
+            if (resultSet.next()) {
+                return resultSet.getInt(1);
+            }
+            return null;
+        });
+        return initializeInstanceDuration;
+    }
+
+    @Override
+    protected Integer doGetSelfProtectedDuration() {
+        String sql = StringPlaceholderResolver.resolvePlaceholder(DEFAULT_QUERY_PLACEHOLDER_SQL, "self_protected_duration");
+
+        Integer selfProtectedDuration = jdbcTemplate.query(sql, args, (resultSet) -> {
+            if (resultSet.next()) {
+                return resultSet.getInt(1);
+            }
+            return null;
+        });
+        return selfProtectedDuration;
+    }
+
+    @Override
+    protected LaunchStatus doGetStatus() {
+        String sql = StringPlaceholderResolver.resolvePlaceholder(DEFAULT_QUERY_PLACEHOLDER_SQL, "status");
+        Short status = jdbcTemplate.query(sql, args, (resultSet) -> {
+            if (resultSet.next()) {
+                return resultSet.getShort(1);
+            }
+            return null;
+        });
+        return LaunchStatus.getEnum(status);
     }
 
     @Override
@@ -223,74 +220,9 @@ public class DBInstanceConfig extends AbstractInstanceConfig {
     }
 
     @Override
-    public Integer getHeartbeatRate() {
-        String sql = StringPlaceholderResolver.resolvePlaceholder(DEFAULT_QUERY_PLACEHOLDER_SQL, "heartbeat_rate");
-
-        Integer heartbeatRate = jdbcTemplate.query(sql, args, (resultSet) -> {
-            if (resultSet.next()) {
-                return resultSet.getInt(1);
-            }
-            return null;
-        });
-
-        heartbeatRate = heartbeatRate != null ? heartbeatRate : applicationConfig.getDefaultHeartbeatRate();
-
-        return heartbeatRate;
-    }
-
-    @Override
-    public Integer getInitializeInstanceDuration() {
-        String sql = StringPlaceholderResolver.resolvePlaceholder(DEFAULT_QUERY_PLACEHOLDER_SQL, "initialize_instance_duration");
-
-        Integer initializeInstanceDuration = jdbcTemplate.query(sql, args, (resultSet) -> {
-            if (resultSet.next()) {
-                return resultSet.getInt(1);
-            }
-            return null;
-        });
-
-        initializeInstanceDuration = initializeInstanceDuration != null ? initializeInstanceDuration : applicationConfig.getDefaultInitializeInstanceDuration();
-
-        return initializeInstanceDuration;
-    }
-
-    @Override
-    public Integer getSelfProtectedDuration() {
-        String sql = StringPlaceholderResolver.resolvePlaceholder(DEFAULT_QUERY_PLACEHOLDER_SQL, "self_protected_duration");
-
-        Integer selfProtectedDuration = jdbcTemplate.query(sql, args, (resultSet) -> {
-            if (resultSet.next()) {
-                return resultSet.getInt(1);
-            }
-            return null;
-        });
-
-        selfProtectedDuration = selfProtectedDuration != null ? selfProtectedDuration : applicationConfig.getDefaultSelfProtectedDuration();
-
-        return selfProtectedDuration;
-    }
-
-    @Override
     public void setStatus(LaunchStatus launchStatus) {
         String sql = StringPlaceholderResolver.resolvePlaceholder(DEFAULT_UPDATE_PLACEHOLDER_SQL, "status");
         jdbcTemplate.update(sql, launchStatus.status, instanceId);
     }
 
-    @Override
-    public LaunchStatus getStatus() {
-        LaunchStatus applicationConfigStatus = applicationConfig.getStatus();
-        if (applicationConfigStatus.equals(LaunchStatus.SHUTDOWN)) {
-            return LaunchStatus.SHUTDOWN;
-        }
-        String sql = StringPlaceholderResolver.resolvePlaceholder(DEFAULT_QUERY_PLACEHOLDER_SQL, "status");
-
-        Short status = jdbcTemplate.query(sql, args, (resultSet) -> {
-            if (resultSet.next()) {
-                return resultSet.getShort(1);
-            }
-            return null;
-        });
-
-        return LaunchStatus.getEnum(status);
-    }
 }
